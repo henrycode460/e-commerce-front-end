7b891ee4dc89dc73026b56475d7d7ee33dc61f5e
Henry <dokinruss@gmail.com>
2023-09-08 12:24:28 +0000

    ecommerce feature

--- components/info.tsx
+++ components/info.tsx
@@ -5,18 +5,18 @@ import { ShoppingCart } from "lucide-react";
 import Currency  from "@/components/ui/currency";
 import Button from "@/components/ui/button";
 import { Product } from "@/types";
-// import useCart from "@/hooks/use-cart";
+import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
   data: Product
 };
 
 const Info: React.FC<InfoProps> = ({ data }) => {
-//   const cart = useCart();
+  const cart = useCart();
 
-//   const onAddToCart = () => {
-//     cart.addItem(data);
-//   }
+  const onAddToCart = () => {
+    cart.addItem(data);
+  }
 
   return ( 
     <div>
@@ -39,12 +39,12 @@ const Info: React.FC<InfoProps> = ({ data }) => {
           <div className="h-6 w-6 rounded-full border border-gray-600" style={{ backgroundColor: data?.color?.value }} />
         </div>
       </div>
-      {/* <div className="mt-10 flex items-center gap-x-3">
+      <div className="mt-10 flex items-center gap-x-3">
         <Button onClick={onAddToCart} className="flex items-center gap-x-2">
           Add To Cart
           <ShoppingCart size={20} />
         </Button>
-      </div> */}
+      </div>
     </div>
   );
 }
